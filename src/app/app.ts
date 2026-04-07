506f001db59ec2b86a1c4501b8d0e89c1086981a
Muhammad Ibrahim <dev.ibrahim0077@gmail.com>
2026-04-07 16:52:33 +0500

    froms with ngrx

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { formFeature } from './form/form.feature';
+import { resetForm, updateFormField } from './form/form.actions';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('ngrx-with-forms');
+  // through obervable and constructor
+  // form$: Observable<{ name: string; email: string }>;
+
+  // constructor(private store: Store) {
+  //   this.form$ = this.store.select(formFeature.selectFormState);
+  // }
+
+  // through inject
+  private store = inject(Store);
+  form$ = this.store.select(formFeature.selectFormState);
+
+  updateField(field: 'name' | 'email', value: string) {
+    this.store.dispatch(updateFormField({ field, value }));
+  }
+
+  resetForm() {
+    this.store.dispatch(resetForm());
+  }
 }
